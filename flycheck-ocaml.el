flycheck/flycheck-ocaml

commit acdd970fdf6685a4aec7b04a9a2236fc483c2b23

Bump let-alist and use nested alist bindings

flycheck-ocaml.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/flycheck/flycheck-ocaml
 ;; Keywords: convenience, tools, languages
 ;; Version: 0.2-cvs
-;; Package-Requires: ((emacs "24.1") (flycheck "0.22-cvs1") (merlin "2.0") (let-alist "1.0.1"))
+;; Package-Requires: ((emacs "24.1") (flycheck "0.22-cvs1") (merlin "2.0") (let-alist "1.0.3"))
 
 ;; This file is not part of GNU Emacs.
 
@@ -76,18 +76,15 @@ Return the corresponding `flycheck-error'."
   (let-alist alist
     (when .message
       (pcase-let* ((`(,level . ,message)
-                    (flycheck-ocaml-merlin-parse-message .message))
-                   (message (or message .message))
-                   (level (or level 'error)))
-        (let-alist .start
-          ;; OCaml columns seem to be zero-based, see
-          ;; https://github.com/flycheck/flycheck-ocaml/issues/2
-          (flycheck-error-new-at (or .line 1)
-                                 (when .col (1+ .col))
-                                 level message
-                                 :checker checker
-                                 :buffer buffer
-                                 :filename (buffer-file-name)))))))
+                    (flycheck-ocaml-merlin-parse-message .message)))
+        ;; OCaml columns seem to be zero-based, see
+        ;; https://github.com/flycheck/flycheck-ocaml/issues/2
+        (flycheck-error-new-at (or .start.line 1)
+                               (when .start.col (1+ .start.col))
+                               (or level 'error) (or .message message)
+                               :checker checker
+                               :buffer buffer
+                               :filename (buffer-file-name))))))
 
 (defun flycheck-verify-ocaml-merlin (_checker)
   "Verify the OCaml Merlin syntax checker."

commit 23a513ba010c999e0c4d9e82a732c22ef9108153

Fix message
Regression from acdd970

flycheck-ocaml.el
@@ -81,7 +81,7 @@ Return the corresponding `flycheck-error'."
         ;; https://github.com/flycheck/flycheck-ocaml/issues/2
         (flycheck-error-new-at (or .start.line 1)
                                (when .start.col (1+ .start.col))
-                               (or level 'error) (or .message message)
+                               (or level 'error) (or message .message)
                                :checker checker
                                :buffer buffer
                                :filename (buffer-file-name))))))