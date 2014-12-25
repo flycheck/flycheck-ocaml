24b4ba10a406fbaa1cea9de76860378b109c7137
Sebastian Wiesner <swiesner@lunaryorn.com>
2014-12-25 14:47:44 +0100

    Bump version in master

--- flycheck-ocaml.el
+++ flycheck-ocaml.el
@@ -5,7 +5,7 @@
 ;; Author: Sebastian Wiesner <swiesner@lunaryorn.com>
 ;; URL: https://github.com/flycheck/flycheck-ocaml
 ;; Keywords: convenience, tools, languages
-;; Version: 0.2
+;; Version: 0.3-cvs
 ;; Package-Requires: ((emacs "24.1") (flycheck "0.22-cvs1") (merlin "2.0") (let-alist "1.0.3"))
 
 ;; This file is not part of GNU Emacs.
