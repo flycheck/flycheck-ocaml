e726ec006316c46cf1a7c3142ac8743191495a3c
Mark Laws <mdl@60hz.org>
2017-07-31 08:21:57 +0900

    Fix travis; add more Emacs versions

--- test/flycheck-ocaml-test.el
+++ test/flycheck-ocaml-test.el
@@ -26,6 +26,28 @@
 
 ;;; Code:
 
+(unless (fboundp 'define-error)
+  ;; from Emacs `subr.el'
+  (defun define-error (name message &optional parent)
+    "Define NAME as a new error signal.
+MESSAGE is a string that will be output to the echo area if such an error
+is signaled without being caught by a `condition-case'.
+PARENT is either a signal or a list of signals from which it inherits.
+Defaults to `error'."
+    (unless parent (setq parent 'error))
+    (let ((conditions
+           (if (consp parent)
+               (apply #'append
+                      (mapcar (lambda (parent)
+                                (cons parent
+                                      (or (get parent 'error-conditions)
+                                          (error "Unknown signal `%s'" parent))))
+                              parent))
+             (cons parent (get parent 'error-conditions)))))
+      (put name 'error-conditions
+           (delete-dups (copy-sequence (cons name conditions))))
+      (when message (put name 'error-message message)))))
+
 (require 'flycheck-ocaml)
 (require 'flycheck-ert)
 (require 'tuareg)
